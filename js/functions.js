199c412c50607821e25082d312497ebcec2b096f
Bruno Rocha <mail.brunorocha@gmai.com>
2018-01-27 09:31:48 -0300

    Adição de responsividade ao menu lateral completa.

--- js/functions.js
+++ js/functions.js
@@ -1,5 +1,43 @@
 $(function() {
+
+    var offCanvasOn = false;
+
+    offCanvasNavActivate();
+    $('.mask').hide();
+
     $('.toggle-nav-left').on('click', function() {
-        $('.nav-left').toggleClass('nav-left-active');
+        $('.nav-left').addClass('nav-left-active')
+        $('.mask').show();
+        offCanvasOn = true;
     });
+
+    $('.mask').on('click', function() {
+        $('.nav-left').removeClass('nav-left-active')
+        $('.mask').hide();
+        offCanvasOn = false;
+    });
+
+    $(window).resize(function() {
+        offCanvasNavActivate();
+    })
+
+    function offCanvasNavActivate() {
+        if(window.innerWidth >= 960) {
+            $('.nav-left').addClass('nav-left-active')
+            $('.nav-left').removeClass('position-fixed')
+            $('.toggle-nav-left').parent().hide();
+
+            offCanvasOn = false;
+            $('.mask').hide();
+        } else {
+            if(offCanvasOn) {
+                $('.mask').show();
+            } else {
+                $('.nav-left').removeClass('nav-left-active')
+            }
+
+            $('.toggle-nav-left').parent().show();
+            $('.nav-left').addClass('position-fixed')
+        }
+    }
 })
