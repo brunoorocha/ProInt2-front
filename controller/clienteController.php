a2b49ddb07e6cf4aeaf9a4ac8c15400d65ae0a14
Victoria Ricarte Bispo Beserra <36166925+vrbb@users.noreply.github.com>
2018-02-19 11:23:53 -0300

    Add files via upload
    CRUDs de cliente e produto, ainda não testados.

---
+++ controller/clienteController.php
@@ -0,0 +1,106 @@
+<?php
+    
+    class clienteController{
+    
+        public function __construct() {
+            
+            require_once 'model/clienteDAO.class.php';
+            require_once 'view/clienteView.php';
+            require_once 'model/cliente.class.php';
+            
+        }
+        
+        
+        function cadastrar(){
+            $cliente = new cliente();
+            if($_SERVER['REQUEST_METHOD']=='POST'){
+                $cliente->setNome($_POST['nome']);
+                $cliente->setProfissao($_POST['profissao']);
+                $cliente->setEndereco($_POST['endereco']);
+                $cliente->setRg($_POST['rg']);
+                $cliente->setCpf($_POST['cpf']);
+                $cliente->setFiliacao($_POST['filiacao']);
+                $cliente->setNaturalidade($_POST['naturalidade']);
+                $cliente->setData_nasc($_POST['data_nasc']);
+                $cliente->setNome_conjuge($_POST['nome_conjuge']);
+                $cliente->setProfissao_conjuge($_POST['profissao_conjuge']);
+                $cliente->setReferencia($_POST['referencia']);
+                $cliente->setTelefone_referencia($_POST['telefone_referencia']);
+
+                $cDAO = new clienteDAO();
+                $cDAO->cadastrarCliente($cliente);
+                echo "OK";
+            } 
+        }
+        
+        function view_cadastrar(){
+          
+            $cVIEW = new cadastroView();
+            $cVIEW->cadastrarCliente();
+          
+        }
+
+        function alterar(){
+            $cDAO = new clienteDAO();
+            $lista = $cDAO->listaTudo();		
+            $cliente = new cliente();
+            $cVIEW = new clienteView();
+            
+             if($_SERVER['REQUEST_METHOD']=='POST'){
+
+                if(isset($_POST['cod_cliente'])){
+                    $cod_cliente = $_POST['cod_cliente'];
+                    $cliente = $cDAO->listaUm($cod_cliente);
+                    $_REQUEST['cliente']=$cliente;
+                    $_REQUEST['lista']=$lista;
+                    $ccVIEW->editarCliente();
+                }
+                if(isset($_POST['cod_cliente']) && $_POST['cod_cliente']!= NULL){
+                    $cliente->setNome($_POST['nome']);
+                    $cliente->setProfissao($_POST['profissao']);
+                    $cliente->setEndereco($_POST['endereco']);
+                    $cliente->setRg($_POST['rg']);
+                    $cliente->setCpf($_POST['cpf']);
+                    $cliente->setFiliacao($_POST['filiacao']);
+                    $cliente->setNaturalidade($_POST['naturalidade']);
+                    $cliente->setData_nasc($_POST['data_nasc']);
+                    $cliente->setNome_conjuge($_POST['nome_conjuge']);
+                    $cliente->setProfissao_conjuge($_POST['profissao_conjuge']);
+                    $cliente->setReferencia($_POST['referencia']);
+                    $cliente->setTelefone_referencia($_POST['telefone_referencia']);
+                    $cliente->setCod_cliente($_POST['cod_cliente']);
+
+                    $cDAO->editarCliente($cliente);
+                    $cVIEW->redirecionar("View_Alterar", "MODIFICAÇÃO BEM SUCEDIDA !!");
+                }
+            }
+        }
+		
+        function View_Alterar(){
+            $cDAO = new clienteDAO();
+            $lista = $cDAO->listaTudo();
+            $_REQUEST['lista']=$lista;
+			
+            $cVIEW = new clienteView();
+            $cVIEW->editarCliente();
+        }
+		
+        function excluir(){
+			
+            $cliente = new cliente();
+            if($_SERVER['REQUEST_METHOD']=='POST'){
+                $cliente->setCod_cliente($_POST['cod_cliente']);
+                $cDAO = new clienteDAO();
+                $cDAO->excluirCliente($cliente);
+                echo "OK";
+            }
+        }
+		
+	function View_Excluir(){
+            $cVIEW = new clienteView();
+            $cVIEW->excluirCliente();
+	}
+            
+    }       
+        
+        
---
+++ controller/produtoController.php
@@ -0,0 +1,91 @@
+<?php
+
+ class produtoController{
+    
+        public function __construct() {
+            
+            require_once 'model/produtoDAO.class.php';
+            require_once 'view/produtoView.php';
+            require_once 'model/produto.class.php';
+            
+        }
+        
+        
+        function cadastrar(){
+            $produto = new produto();
+            if($_SERVER['REQUEST_METHOD']=='POST'){
+                $produto->setNome($_POST['nome']);
+                $produto->setPreco_fabrica($_POST['preco_fabrica']);
+                $produto->setPreco_revenda($_POST['preco_revenda']);
+                $produto->setFornecedor($_POST['fornecedor']);
+                
+
+                $pDAO = new produtoDAO();
+                $pDAO->cadastrarCliente($produto);
+                echo "OK";
+            } 
+        }
+        
+        function view_cadastrar(){
+          
+            $pVIEW = new cadastroView();
+            $pVIEW->cadastrarProduto();
+          
+        }
+
+        function alterar(){
+            $pDAO = new produtoDAO();
+            $lista = $pDAO->listaTudo();		
+            $produto = new produto();
+            $pVIEW = new produtoView();
+            
+             if($_SERVER['REQUEST_METHOD']=='POST'){
+
+                if(isset($_POST['cod_produto'])){
+                    $cod_produto = $_POST['cod_produto'];
+                    $produto = $pDAO->listaUm($cod_produto);
+                    $_REQUEST['produto']=$produto;
+                    $_REQUEST['lista']=$lista;
+                    $pVIEW->editarProduto();
+                }
+                if(isset($_POST['cod_produto']) && $_POST['cod_produto']!= NULL){
+                    $produto->setNome($_POST['nome']);
+                	$produto->setPreco_fabrica($_POST['preco_fabrica']);
+                	$produto->setPreco_revenda($_POST['preco_revenda']);
+                	$produto->setFornecedor($_POST['fornecedor']);
+                    $produto->setCod_produto($_POST['cod_produto']);
+
+                    $pDAO->editarProduto($produto);
+                    $pVIEW->redirecionar("View_Alterar", "MODIFICAÇÃO BEM SUCEDIDA !!");
+                }
+            }
+        }
+		
+        function View_Alterar(){
+            $pDAO = new produtoDAO();
+            $lista = $pDAO->listaTudo();
+            $_REQUEST['lista']=$lista;
+			
+            $pVIEW = new produtoView();
+            $pVIEW->editarProduto();
+        }
+		
+        function excluir(){
+			
+            $produto = new produto();
+            if($_SERVER['REQUEST_METHOD']=='POST'){
+                $produto->setCod_produto($_POST['cod_produto']);
+                $pDAO = new produtoDAO();
+                $pDAO->excluirProduto($produto);
+                echo "OK";
+            }
+        }
+		
+	function View_Excluir(){
+            $pVIEW = new produtoView();
+            $pVIEW->excluirProduto();
+	}
+            
+    }       
+        
+
