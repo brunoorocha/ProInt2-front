2e3d0b38fff7c62499c3bddd64d288afc699117a
Victoria Ricarte Bispo Beserra <36166925+vrbb@users.noreply.github.com>
2018-02-26 11:18:46 -0300

    Add files via upload
    Alteração no arquivo funcionarioDAO.class.php
    Adição da função de Logar no sistema

--- model/funcionarioDAO.class.php
+++ model/funcionarioDAO.class.php
@@ -54,5 +54,31 @@ class funcionarioDAO{
         $pdo = $conex->conecta();
         $stmt = $pdo->query($q);
     }
+
+    function loginFuncionario($fun){
+        $login = $fun->getLogin();
+        $senha = $fun->getSenha();
+
+        $conexao = new conexao("localhost", "root", "", "otica");
+        $PDO = $conexao->conecta();
+        $stmt = $PDO->query("SELECT * FROM funcionario");
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        for($i = 0; $i < $stmt->rowCount(); $i++){
+            $funcionario[$i] = new funcionario();
+            $funcionario[$i]->setLogin($result[$i]['login']);
+            $funcionario[$i]->setSenha($result[$i]['senha']);
+            $funcionario[$i]->setCod_funci($result[$i]['cod_funci']);
+
+        }
+        foreach ($funcionario as $linha) {
+                  $loginO = $linha->getLogin();
+                  $senhaO = $linha->getSenha();
+                  if ($loginO == $login && $senhaO == $senha) {
+                      echo "<h1>Usuário logado com sucesso!</h1>";
+                  }else{
+                    echo "<h1>Usuário ou senha incorretos!</h1>";
+                  }
+           }
+    }
     
 }
