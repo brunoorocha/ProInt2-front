2a48fdbc642f69a80fd1a966ff74fee7f0992803
Victoria Ricarte Bispo Beserra <36166925+vrbb@users.noreply.github.com>
2018-03-12 09:18:19 -0300

    Add files via upload
    Revsão de vendaDAO, alterações em todos os métodos para conexão e métodos com retorno usando json.

--- api/model/vendaDAO.php
+++ api/model/vendaDAO.php
@@ -16,53 +16,65 @@ class vendaDAO{
         
         
         $q = "INSERT INTO venda (forma_pagamento,qtd_parcela,obs,cliente_cod,produto_cod, data) VALUES('$forma_pagamento','$qtd_parcela','$obs','$cliente_cod','$produto_cod','$data')";   
-        $conex = new conexao("localhost", $MYSQL_USER, $MYSQL_PASS, "otica");
-        $pdo = $conex->conecta();
-        $stmt = $pdo->query($q);
+        $conex = conexao::connect();        
+        $stmt = $conex->query($q);
     }
 
-    function listaTudo(){
+    function retornaVendas(){
         
-        $q = "SELECT * FROM venda";
-        $conex = new conexao("localhost", $MYSQL_USER, $MYSQL_PASS, "otica");
-        $pdo = $conex->conecta();
-        $stmt = $pdo->query($q);
+        $conexao = conexao::connect();        
+        $stmt = $conexao->query("SELECT * FROM venda");        
         $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        $vendas = array();
+
+        if($stmt->rowCount() == 0) {            
+            return NULL;
+        }
 		
         for($i=0; $i<$stmt->rowCount(); $i++){
-            $venda[$i]= new venda(); 
-            $venda[$i]->setCod_venda($result[$i]['cod_venda']);
-            $venda[$i]->setForma_pagamento($result[$i]['forma_pagamento']);
-            $venda[$i]->setQtd_parcela($result[$i]['qtd_parcela']);
-            $venda[$i]->setObs($result[$i]['obs']);
-            $venda[$i]->cliente->setCod_cliente($result[$i]['cliente_cod']);
-            $venda[$i]->produto->setCod_produto($result[$i]['produto_cod']);
-            $venda[$i]->setData($result[$i]['data']);
+            $venda = array();
+            $venda["cod_venda"]=$result[$i]['cod_venda'];
+            $venda["forma_pagamento"]=$result[$i]['forma_pagamento'];
+            $venda["qtd_parcela"]=$result[$i]['qtd_parcela'];
+            $venda["obs"]=$result[$i]['obs'];
+            $venda["cliente_cod"]=$result[$i]['cliente_cod'];
+            $venda["produto_cod"]=$result[$i]['produto_cod'];
+            $venda["data"]=$result[$i]['data'];
+
+            array_push($vendas, $venda);
         }
+
         
-	return $venda;
+	return json_encode($vendas);
 	}
 
     // Função que retorna as informações de 1 produto passando como parâmetro o seu código
         
     function listaUm($cod_venda){
         
-        $q = "SELECT * FROM venda WHERE cod_venda = $cod_venda";
-        $conex = new conexao("localhost", $MYSQL_USER, $MYSQL_PASS, "otica");
-        $pdo = $conex->conecta();
-        $stmt = $pdo->query($q);
+    
+        $conexao = conexao::connect();        
+        $stmt = $conexao->query("SELECT * FROM venda WHERE cod_venda = $cod_venda"); 
         $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-        $venda= new venda(); 
-        $venda->setCod_venda($result[0]['cod_venda']);
-        $venda->setForma_pagamento($result[0]['forma_pagamento']);
-        $venda->setQtd_parcela($result[0]['qtd_parcela']);
-        $venda->setObs($result[0]['obs']);
-        $venda->cliente->setCod_cliente($result[0]['cliente_cod']);
-        $venda->produto->setCod_produto($result[0]['produto_cod']);
-        $venda->setData($result[0]['data']);
+        
 
-	return $venda;
+        if($stmt->rowCount() == 0) {            
+            return NULL;
+        }
+        
+        
+            $venda = array();
+            $venda["cod_venda"]=$result[$i]['cod_venda'];
+            $venda["forma_pagamento"]=$result[$i]['forma_pagamento'];
+            $venda["qtd_parcela"]=$result[$i]['qtd_parcela'];
+            $venda["obs"]=$result[$i]['obs'];
+            $venda["cliente_cod"]=$result[$i]['cliente_cod'];
+            $venda["produto_cod"]=$result[$i]['produto_cod'];
+            $venda["data"]=$result[$i]['data'];    
+            return json_encode($venda);
+	
     }
     function editarVenda($venda){
         $cod_venda=$venda->getCod_venda();
@@ -72,20 +84,15 @@ class vendaDAO{
         $cliente_cod=$venda->cliente->getCod_cliente();
         $produto_cod=$venda->produto->getCod_produto();
 
-        $this->conecta();
+        
         $q = "UPDATE venda set forma_pagamento='$forma_pagamento',qtd_parcela='$qtd_parcela',obs='$obs',cliente_cod='$cliente_cod',produto_cod='$produto_cod' WHERE cod_venda=$cod_venda";   
-        $conex = new conexao("localhost", $MYSQL_USER, $MYSQL_PASS, "otica");
-        $pdo = $conex->conecta();
-        $stmt = $pdo->query($q);
+        $conex = conexao::connect();        
+        $stmt = $conex->query($q);
     }
 
 
-    function excluirVenda($venda){
-        $cod_venda=$venda->getCod_venda();
-        $this->conecta();
-        $q = "DELETE FROM venda WHERE cod_venda=$cod_venda";
-        $conex = new conexao("localhost", $MYSQL_USER, $MYSQL_PASS, "otica");
-        $pdo = $conex->conecta();
-        $stmt = $pdo->query($q);
+    function excluirVenda($cod_venda){
+        $conexao = conexao::connect();
+        $stmt = $conex->query("DELETE FROM venda WHERE cod_venda=$cod_venda");
     }
 }
